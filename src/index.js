8d1a7f8c9b61c60fd5f36ec489108c8fc8173e04
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-16 15:26:22 +0300

    set up Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./Components/App/App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { rootReducer } from "./Redux/Reducers/rootReducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer); // rootReducer () ?
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
